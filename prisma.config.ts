bbc268c4312d2d70079118564fdd9860a2681c94
laknes <rezajanami@gmail.com>
2026-07-18 01:46:04 +0330

    prismaa

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,8 +1,3 @@
-import { defineConfig } from "prisma/config";
-
-export default defineConfig({
+module.exports = {
   schema: "prisma/schema.prisma",
-  datasource: {
-    url: process.env.DATABASE_URL ?? "postgresql://postgres:postgres@127.0.0.1:5432/amadeh_pack?schema=public",
-  },
-});
+};
